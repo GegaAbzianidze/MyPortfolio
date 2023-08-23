266104b983e317f1ef9844ad404c1405e4da58d8
Gega <gega.abzianidze.1@btu.edu.ge>
2023-08-23 18:42:20 +0400

    Google Analitics

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,35 @@
 import "./App.css";
 import Loading from "./Assets/Loading";
 import NavBar from "./Components/NavBar";
-import { lazy, Suspense } from "react";
+import { Component, lazy, Suspense } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import ReactGA from "react-ga";
 
 const Home = lazy(() => import("./Pages/Home"));
 const CV = lazy(() => import("./Pages/CV"));
 
-function App() {
-  return (
-    <div className=" no-auto">
-      <Router>
-        <Suspense fallback={<Loading />}>
-          <NavBar />
-          <Routes>
-            <Route path="/" element={<Home />} exact />
-            <Route path="/CV" element={<CV />} />
-          </Routes>
-        </Suspense>
-      </Router>
-    </div>
-  );
+ReactGA.initialize("G-4T2K7C0FRH");
+
+class App extends Component {
+  componentDidMount() {
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }
+
+  render() {
+    return (
+      <div className=" no-auto">
+        <Router>
+          <Suspense fallback={<Loading />}>
+            <NavBar />
+            <Routes>
+              <Route path="/" element={<Home />} exact />
+              <Route path="/CV" element={<CV />} />
+            </Routes>
+          </Suspense>
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
